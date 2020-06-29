f532032a8d4c9e05a1f00d57e0b6e2a87dfc1032
Emmanuel Cartier <emmanuel.cartier@lipn.univ-paris13.fr>
2020-06-29 18:13:06 +0200

    clean up!

--- lib/treetagger_test.py
+++
@@ -1,7 +0,0 @@
-import treetaggerwrapper as ttpw
-tagger = ttpw.TreeTagger(TAGLANG='fr', TAGOPT="-prob -threshold 0.7 -token -lemma -sgml")
-tags = tagger.tag_text('Voici un petit test de TreeTagger pour voir.')
-import pprint
-pprint.pprint(tags)
-tags2 = ttpw.make_tags(tags, allow_extra=True)
-pprint.pprint(tags2)
